a2445b0791382b02d56f7adebd5d57937e5e78ee
gonebone <kunalkumar2938@gmail.com>
2025-10-22 01:26:03 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+
+import { Button } from "@/components/ui/button";
+
+
+const Home = () => {
+  return (
+    <div className="flex min-h-screen home-wrapper">
+      
+      Home
+
+    </div>
+  )
+}
+
+export default Home;
---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+ export const NAV_ITEMS: { href: string; label: string }[] = [
+    {href: '/', label: 'Dashboard'},
+    {href: '/serach', label: 'Search'},
+    {href: '/watchlist', label: 'Watchlist'},
+];
